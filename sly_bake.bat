db0c0a4918dc597c75c0fe0c04d4d59bbaeab0ef
Nick Renieris <velocityra@gmail.com>
2020-12-07 00:43:18 +0200

    sly_bake: Fix psarc command & add filelist for Sly 1

--- sly_bake.bat
+++ sly_bake.bat
@@ -8,13 +8,20 @@ set PSARC="D:\Nikos\Reversing\Tools\PS3 Tools\psarc.exe"
 
 REM Replace 'y' below with nothing to disable compressing the level below
 set ENABLE_COMPRESS_LEVEL=y
-set LEVEL_PATH=D:\Nikos\Reversing\Sly\Code\sly_playground\tests\jb_intro_W.dec
+set LEVEL_PATH=D:\Nikos\Reversing\Sly\Code\SlyCooper\tests\jb_intro_W.dec
 set LEVEL_NAME_FINAL=jb_intro_W
 
 if defined ENABLE_COMPRESS_LEVEL (
-    %SLY_TOOLS%\sly_compressor.exe "%LEVEL_DIR%\%LEVEL_NAME%" "%SLY1_DATA%\%LEVEL_NAME_FINAL%"
+    echo --- Compressing level %LEVEL_NAME_FINAL% ---
+    %SLY_TOOLS%\sly_compressor.exe %LEVEL_PATH% %SLY1_DATA%\%LEVEL_NAME_FINAL%
 )
 
+echo --- Packing to .WAC/.WAL ---
 %SLY_TOOLS%\sly_packer.exe "%SLY1_DATA%" %SLY1_ROOT%
 
+Robocopy %SLY_USRDIR% %CD%\data\Sly1_filelist.txt > nul
+
+echo --- Packaging/compressing to .psarc ---
+pushd %SLY_USRDIR%
 %PSARC% create -y --output=%SLY_USRDIR%\Sly1.psarc --inputfile=%SLY_USRDIR%\Sly1_filelist.txt
+popd
