2459f09402508a4f497d28e3904034120448f079
Nick Renieris <velocityra@gmail.com>
2020-12-07 23:13:01 +0200

    packer: Support optionally specifying output WAC/WAL paths

--- src/sly_packer.cpp
+++ src/sly_packer.cpp
@@ -11,22 +11,21 @@
 int main(int argc, char *argv[]) {
     try {
         if (argc <= 2)
-            throw std::runtime_error(std::string(argv[0]) + " <input_dir> <output_path>");
+            throw std::runtime_error(std::string(argv[0]) + " <input_dir> [<output_file_wac> <output_file_wal>]");
 
         const std::filesystem::path input_dir_path = argv[1];
-        const std::filesystem::path output_dir_path = argv[2];
 
-        const auto wac_path = output_dir_path / "SLY.WAC";
+        const auto wac_path = (argc == 4) ? argv[2] : input_dir_path / "SLY.WAC";
         std::ofstream wac_ofs(wac_path, std::ios::binary | std::ios::trunc);
 
-        const auto wal_path = output_dir_path / "SLY.WAL";
+        const auto wal_path = (argc == 4) ? argv[3] : input_dir_path / "SLY.WAL";
         std::ofstream wal_ofs(wal_path, std::ios::binary | std::ios::trunc);
 
         u32 wal_size{};
         u64 wal_next_sector_offset{};
-
         u32 wac_entry_count{};
 
+        // We'll write the entry count here later
         stream_write(wac_ofs, (u32) 0);
 
 #ifdef NDEBUG
